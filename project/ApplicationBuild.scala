1955dc4f1853ca8fa000403bcebcf0a2b7c5ffca
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-11-05 23:22:05 +0000

    Adding acceptance test phase

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -7,7 +7,7 @@ object ApplicationBuild extends Build {
   val appName = "surfersTV"
   val appVersion = envOrElse("TV_API_VIEW_VERSION", "999-SNAPSHOT")
 
-  def unitFilter(name: String): Boolean = !integrationFilter(name)
+  def unitFilter(name: String): Boolean = !(integrationFilter(name) && acceptanceFilter(name))
   def integrationFilter(name: String): Boolean = name contains "Int"
   def acceptanceFilter(name: String): Boolean = name contains "Feature"
 
@@ -25,7 +25,9 @@ object ApplicationBuild extends Build {
     .settings(inConfig(IntTest)(Defaults.testSettings) : _*)
     .settings(scalaSource in Test <<= baseDirectory(_ / "test/unit"),
       testOptions in Test := Seq(Tests.Filter(unitFilter)),
-      testOptions in IntTest := Seq(Tests.Filter(integrationFilter)))
+      testOptions in IntTest := Seq(Tests.Filter(integrationFilter)),
+      testOptions in AccTest := Seq(Tests.Filter(acceptanceFilter)))
+    .settings(scalaSource in AccTest <<= baseDirectory(_ / "test/acceptance"))
     .settings(scalaSource in IntTest <<= baseDirectory(_ / "test/integration"))
     .settings(libraryDependencies ++= Seq("org.scalatestplus" % "play_2.10" % "1.0.0" % "test"))
     .settings(Keys.fork in (Test) := false)
