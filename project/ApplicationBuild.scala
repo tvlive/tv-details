c41f51285055c44ec02a8f5f17264d705f2e52aa
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-11-07 19:27:15 +0000

    Adding runner for cucumber and build configuration

--- project/ApplicationBuild.scala
+++ project/ApplicationBuild.scala
@@ -1,19 +1,20 @@
+import sbt.Keys._
 import sbt._
-import Keys._
+
 import scala.util.Properties._
-import templemore.sbt.cucumber.CucumberPlugin
 
 object ApplicationBuild extends Build {
 
   val appName = "surfersTV"
   val appVersion = envOrElse("TV_API_VIEW_VERSION", "999-SNAPSHOT")
 
-  def unitFilter(name: String): Boolean = !(integrationFilter(name) && acceptanceFilter(name))
   def integrationFilter(name: String): Boolean = name contains "Int"
-  def acceptanceFilter(name: String): Boolean = name contains "Feature"
+  def acceptanceFilter(name: String): Boolean = name contains "FeaturesRunner"
+  def unitFilter(name: String): Boolean =
+    !integrationFilter(name) && !acceptanceFilter(name)
 
-  lazy val IntTest = config("integration") extend(Test)
-  lazy val AccTest = config("acceptance") extend(Test)
+  lazy val IntTest = config("integration") extend (Test)
+  lazy val AccTest = config("acceptance") extend (Test)
 
   val appDependencies = Seq(
     "org.reactivemongo" %% "reactivemongo" % "0.10.0"
@@ -22,24 +23,30 @@ object ApplicationBuild extends Build {
   val buildSettings = Defaults.defaultSettings
 
   val main = play.Project(appName, appVersion, appDependencies)
-    .configs( IntTest )
-    .settings(inConfig(IntTest)(Defaults.testSettings) : _*)
     .settings(scalaSource in Test <<= baseDirectory(_ / "test/unit"),
-      testOptions in Test := Seq(Tests.Filter(unitFilter)),
-      testOptions in IntTest := Seq(Tests.Filter(integrationFilter)),
-      testOptions in AccTest := Seq(Tests.Filter(acceptanceFilter)))
+      testOptions in Test := Seq(Tests.Filter(unitFilter)))
+
+    .configs(IntTest)
+    .settings(inConfig(IntTest)(Defaults.testSettings): _*)
+    .settings(testOptions in IntTest := Seq(Tests.Filter(integrationFilter)))
     .settings(scalaSource in IntTest <<= baseDirectory(_ / "test/integration"))
     .settings(libraryDependencies ++= Seq("org.scalatestplus" % "play_2.10" % "1.0.0" % "test"))
     .settings(Keys.fork in (Test) := false)
-    .configs( AccTest )
-    .settings(inConfig(AccTest)(Defaults.testSettings) : _*)
-    .settings(scalaSource in IntTest <<= baseDirectory(_ / "test/acceptance"))
-    .settings(libraryDependencies ++= Seq("org.scalatestplus" % "play_2.10" % "1.0.0" % "test"))
-    .settings(CucumberPlugin.cucumberSettings: _*)
-    .settings(
-      CucumberPlugin.cucumberFeaturesLocation := "cucumber",
-      CucumberPlugin.cucumberStepsBasePackage := "cucumber.steps"
-    )
+
+    .configs(AccTest)
+    .settings(testOptions in AccTest := Seq(Tests.Filter(acceptanceFilter)))
+    .settings(inConfig(AccTest)(Defaults.testSettings): _*)
+    .settings(scalaSource in AccTest <<= baseDirectory(_ / "test/acceptance"))
+    .settings(libraryDependencies ++= Seq("org.scalatestplus" % "play_2.10" % "1.0.0" % "test",
+    "info.cukes" %% "cucumber-scala" % "1.1.8" % "test",
+    "info.cukes" % "cucumber-junit" % "1.1.8" % "test",
+    "junit" % "junit" % "4.11" % "test"))
+
+  //    .settings(CucumberPlugin.cucumberSettings: _*)
+  //    .settings(
+  //      CucumberPlugin.cucumberFeaturesLocation := "cucumber",
+  //      CucumberPlugin.cucumberStepsBasePackage := "cucumber.steps"
+  //    )
 
 
 }
